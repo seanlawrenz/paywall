dedc18e3dcc53cf7ad9d9f3e211e7e77cc3160e6
Sean Lawrenz <seanlawrenz@megans-mbp.lan>
2021-12-18 09:21:04 -0500

    Update wash po

--- server/scrpperService/index.js
+++ server/scrpperService/index.js
@@ -11,14 +11,13 @@ class ScrapperService {
   washingtonPost(html) {
     const $ = cheerio.load(html);
     const title = $('h1').html();
-    const data = $('[data-qa="main"]').html();
+    const data = $('.story').html();
     return { title, data };
   }
 
   columbusDispatch(html) {
     const $ = cheerio.load(html);
     const title = $('h1').html();
-    console.log(html);
     const data = $('article').html();
     return { title, data };
   }
